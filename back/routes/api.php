16e9e19249d72bff8e18414153b7e913e1b22247
radytrainer <rady.y@passerellesnumeriques.org>
2021-12-06 10:25:44 +0700

    json reader

--- back/routes/api.php
+++ back/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\CountryController;
 
 Route::get('books', [BookController::class, 'getAllBooks']);
 Route::get('books/{id}', [BookController::class, 'getBook']);
@@ -11,3 +12,6 @@ Route::get('books/{id}', [BookController::class, 'getBook']);
 Route::post('books', [BookController::class, 'createBook']);
 Route::put('books/{id}', [BookController::class, 'updateBook']);
 Route::delete('books/{id}', [BookController::class, 'deleteBook']);
+
+
+Route::get('countries', [CountryController::class, 'getCountries']);
